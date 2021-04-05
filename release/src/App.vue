41fa5ae4b9b330cf271f186a45f3f52f4032929c
Игорь Чиёсов <i.chiesov@medved.studio>
2021-04-05 22:07:40 +0700

    release add side bar.

--- release/src/App.vue
+++ release/src/App.vue
@@ -1,13 +1,57 @@
 <template>
-    <div id="app"><liderboard></liderboard></div>
+    <div class="page-container">
+        <liderboard
+            class="liderboard"
+            :class="{ liderboard__active: isMenuOpen }"
+        ></liderboard>
+
+        <button v-on:click="isMenuOpen = !isMenuOpen">Открыть</button>
+        <game-info
+            class="game-info"
+            :class="{ 'game-info__active': isMenuOpen }"
+        ></game-info>
+    </div>
 </template>
 
 <script>
+import GameInfo from './components/Game-Info.vue'
 import Liderboard from './components/Liderboard.vue'
 export default {
     name: 'App',
-    components: { Liderboard },
+    data: function() {
+        return { isMenuOpen: false }
+    },
+    components: { Liderboard, GameInfo },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.game-info {
+    position: absolute;
+    top: 0;
+    left: 75%;
+    z-index: 1;
+    width: 25%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.5s;
+    transform: translateX(100%);
+
+    &__active {
+        transform: translateX(0%);
+    }
+}
+
+.liderboard {
+    transition: 0.5s;
+    position: relative;
+    z-index: 0;
+
+    &__active {
+        //transform: translateX(-30%);
+        max-width: 70%;
+    }
+}
+</style>
---
+++ release/src/components/Game-Info.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="game-info"></div>
+</template>
+<script>
+export default {
+    name: 'Game-Info',
+}
+</script>
+<style lang="scss">
+.game-info {
+    height: 200px;
+    background-color: black;
+}
+</style>
